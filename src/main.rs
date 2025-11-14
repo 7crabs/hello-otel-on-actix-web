9dd39fbf036d77a4aad3f87ab941c98b135e35e4
nk <41434466+7crabs@users.noreply.github.com>
2025-11-15 03:08:10 +0900

    Reapply "Reapply "chore: Add hello world""
    This reverts commit c808ccf6c6e56be2228e87b91775db61b41f1f2e.

--- src/main.rs
+++ src/main.rs
@@ -71,6 +71,7 @@ fn init_telemetry() -> opentelemetry_sdk::trace::SdkTracerProvider {
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
+    println!("hello, world!");
     let provider = init_telemetry();
 
     HttpServer::new(move || {
